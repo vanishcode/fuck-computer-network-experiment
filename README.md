2e8cfde69189deac9d6b9e1b5a9ae24875271a7b
vanishcode <whyvans@gmail.com>
2018-02-06 19:41:40 +0800

    add recvlen.c

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # fuck-computer-network-experiment
 fuck teacher water water water : )
 
+> 操你妈，就他妈操你妈
 
+计划学习时间10d，代码包括n length readline，echo server，select mode等，语言为C语言
 
-计划学习时间10d，代码包括n length readline，echo server，select mode等，
-
-语言为C语言
+目录：
+1.echo server
+2.recvlen
---
+++ recvvl.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+#define BUF_SIZE 100
+
+int main(){
+	
+	return 0;
+}
+
+int recvvl(struct sockaddr_in s,char* recvbuf,unsigned int recvbuflen){
+	int iResult;
+	unsigned int reclen;
+	
+}
